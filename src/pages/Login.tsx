1e1cc750e58bc2c32d7ac121db6efe7112817b24
emess2g <essuonemma84gmail.com>
2025-02-06 13:44:41 +0000

    ui updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
+import google_logo from "../../public/g_logo.png"
 
 const Login = () => {
 	return (
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-white'>
+				<h1 className='text-3xl mb-2 font-semibold text-center text-white'>
 					Login
-					<span className='text-blue-500'> ChatApp</span>
+					<span className='text-blue-500'> Tech Connect</span>
 				</h1>
+				<p className="text-center">Hello, Welcome Back!👋</p>
 
 				<form>
-					<div>
+					{/* <div>
 						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
+						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-12' />
+					</div> */}
+
+					<div>
+						<label className='label p-2 '>
+							<span className='text-base label-text'>Email</span>
+						</label>
+						<input type='text' placeholder='Enter Your Emaill' className='w-full input input-bordered h-12' />
 					</div>
 
 					<div>
@@ -24,19 +33,41 @@ const Login = () => {
 						<input
 							type='password'
 							placeholder='Enter Password'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered h-12'
 						/>
 					</div>
+
+					<div className="flex justify-between mt-4">
+						<div className="">
+						<input type="checkbox" name="" id="" />
+						<span>  Remember Me</span>
+						</div>
+
+						<p className="text-red-500">Forgot Password?</p>
+					</div>
+
+					<div>
+						<button className='btn btn-block btn-md mt-6'>Login</button>
+					</div>
+
+					<div className="flex justify-center my-6 items-center whitespace-nowrap">
+					<hr className="w-[50%] border border-blue-500 mr-50" /> 
+				    	<p className="mx-4">Or With</p>
+					 <hr  className="w-[50%] border border-blue-500"/>
+					</div>
+
+					<div>
+						<button className=' flex justify-start gap-10 btn btn-block btn-md mb-6 border border-slate-700 '>
+							 <img className="w-6 rounded-full" src={google_logo} alt="" />
+							  Signup with Google</button>
+					</div>
+
 					<Link
 						to='/signup'
-						className='text-sm  hover:underline text-white hover:text-blue-600 mt-2 inline-block'
+						className='flex justify-center text-sm  hover:underline text-white hover:text-blue-600 mt-2 text-center'
 					>
 						{"Don't"} have an account?
 					</Link>
-
-					<div>
-						<button className='btn btn-block btn-sm mt-2'>Login</button>
-					</div>
 				</form>
 			</div>
 		</div>
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 // import GenderCheckbox from "../components/GenderCheckbox";
-import GenderCheckbox from "../components/GenderCheckbox";
+// import GenderCheckbox from "../components/GenderCheckbox";
+import gogle_logo from '../../public/g_logo.png'
 
 const SignUp = () => {
 	return (
@@ -9,20 +10,32 @@ const SignUp = () => {
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
 					Sign Up <span className='text-blue-500'> Tech Connect</span>
 				</h1>
+				<p className="text-sm mt-2 text-center mb-2"> Connect with your friends today 👋!</p>
 
 				<form>
-					<div>
+					{/* <div>
 						<label className='label p-2'>
 							<span className='text-base label-text'>Full Name</span>
 						</label>
 						<input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
-					</div>
+					</div> */}
 
 					<div>
 						<label className='label p-2 '>
 							<span className='text-base label-text'>Username</span>
 						</label>
-						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-12' />
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Email</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Your Email'
+							className='w-full input input-bordered h-12'
+						/>
 					</div>
 
 					<div>
@@ -32,33 +45,37 @@ const SignUp = () => {
 						<input
 							type='password'
 							placeholder='Enter Password'
-							className='w-full input input-bordered h-10'
+							className='w-full input input-bordered h-12'
 						/>
+					</div>
+
+					{/* <GenderCheckbox />  */}			
+
+					<div>
+						<button className='btn btn-block btn-md mt-6 border border-slate-700'>Sign Up</button>
+					</div>
+
+					<div className="flex justify-center my-6 items-center whitespace-nowrap">
+					<hr className="w-[50%] border border-blue-500 mr-50" /> 
+				    	<p className="mx-4">Or With</p>
+					 <hr  className="w-[50%] border border-blue-500"/>
 					</div>
 
 					<div>
-						<label className='label'>
-							<span className='text-base label-text'>Confirm Password</span>
-						</label>
-						<input
-							type='password'
-							placeholder='Confirm Password'
-							className='w-full input input-bordered h-10'
-						/>
+						<button className=' flex justify-start gap-10 btn btn-block btn-md  border border-slate-700 '>
+							 <img className="w-6 rounded-full" src={gogle_logo} alt="" />
+							  Signup with Google</button>
 					</div>
 
-					<GenderCheckbox />
-
+				
+			
 					<Link
 						to={"/login"}
-						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block text-white'
+						className='flex justify-center mt-6 text-sm hover:underline hover:text-blue-600   text-white'
 					>
-						Already have an account?
+						Already have an account? 
 					</Link>
-
-					<div>
-						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
-					</div>
+					
 				</form>
 			</div>
 		</div>
