072fe0b24f41c0f170b509e132fd329fda446183
emess2g <essuonemma84gmail.com>
2025-02-06 03:40:50 +0000

    app setup and complete ui design

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import SignUp from "./pages/SignUp"
+import Login from "./pages/Login"
+
+const App = () => {
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+       <Routes>
+         <Route path="/" element={<Home/>}/>
+         <Route path="/signup" element={<SignUp/>}/>
+         <Route path="/login" element={<Login/>}/>
+       </Routes>
+    </div>
+  )
+}
+
+export default App
+
+
