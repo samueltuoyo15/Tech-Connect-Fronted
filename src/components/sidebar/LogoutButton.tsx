530b9e0f48142d94661ae6778115c8d0bc7468b0
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-13 18:54:10 +0100

    integrating the api

--- src/components/sidebar/LogoutButton.tsx
+++ src/components/sidebar/LogoutButton.tsx
@@ -1,13 +1,16 @@
 import { LogOut } from "lucide-react";
 
 const LogoutButton = () => {
-	const logout = () => {
-		alert("You are logged out");
+	const handleLogout = async () => {
+		const response = await fetch("http://localhost:5000/api/auth/logout")
+		const data = await response.json()
+		alert(data?.message)
+		window.location.href = "/"
 	};
 
 	return (
 		<div className='mt-auto'>
-			<LogOut className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
+			<LogOut onClick={() => handleLogout} className='w-6 h-6 text-white cursor-pointer' onClick={logout} />
 		</div>
 	);
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import {useContext} from "react"
+import {AuthContext} from "../context/AuthContext"
 import MessageContainer from "../components/messages/MessageContainer";
 import Sidebar from "../components/sidebar/Sidebar";
 
 const Home = () => {
+  const {authUser, isLoading} = useContext(AuthContext)
+  if(isLoading) return <div>Loading....</div>
 	return (
 		<div className='flex h-[80vh] w-full md:max-w-screen-md md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			<Sidebar />
-			<MessageContainer />
+			<Sidebar user={authUser}/>
+			<MessageContainer user={authUser}/>
 		</div>
 	);
 };
